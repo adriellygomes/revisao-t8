c3bf1365413f8df97dc274fdd2d7f2c62332ad1e
adriellygomes <adriellygomes666@gmail.com>
2019-10-28 13:31:13 -0300

    subindo exercicios resolvidos

--- 00a_revisao/index.js
+++ 00a_revisao/index.js
@@ -1,7 +1,57 @@
 // 1.
-// [] adicionar a tag <script> no html
-// [] verificar se está lendo index.js
-// [] alterar cor de <body>
-// [] escutar o clique do botão
-// [] criar função de alterar a cor usando rgb()
+// [x] adicionar a tag <script> no html
+// [x] verificar se está lendo index.js
+// [x] alterar cor de <body>
+// [x] escutar o clique do botão
+// [x] criar função de alterar a cor usando rgb()
 // [] usar Math.random() e Math.floor() ou parseInt()
+
+console.log("olar");
+
+const body = document.body;
+console.log(body);
+
+const botao = document.getElementById('trocaCor');
+body.style.backgroundColor = "purple"
+
+botao.addEventListener("click", clicouBotao);
+
+function geraInteiro() {
+    const n = parseInt(Math.random() * 255);
+    return n;
+}
+
+
+
+function clicouBotao() {
+    const r = geraInteiro();
+    const g = geraInteiro();
+    const b = geraInteiro();
+    
+    console.log('entrei na função');
+    body.style.backgroundColor = 'rgb(' + r + ',' + g + ' , ' + b + ')'
+    
+    
+}
+
+
+
+
+
+
+// const n = Math.random();
+// const = n2 * 255;
+// const int = parseInt (n2);
+
+// console.log('numero:', n);
+// console.log('numero:', n2);
+// console.log('int', int);
+// const intRandom = parseInt(Math.random() * 255)
+// console.log(intRandom)
+
+// console.log(botao);
+
+// pegar coisas do html -----------------------
+
+// const body = document.querySelector('body');
+// const body = document.getElementsByTagName('body')
--- 00b_revisao/index.js
+++ 00b_revisao/index.js
@@ -6,9 +6,63 @@
 // [] criar função de alterar a cor usando rgb()
 // [] usar Math.random() e Math.floor() ou parseInt()
 
+
+// console.log("olar");
+
+// const body = document.body;
+// console.log(body);
+
+// const botao = document.getElementById('trocaCor');
+// body.style.backgroundColor = "purple"
+
+// botao.addEventListener("click", clicouBotao);
+
+// function geraInteiro() {
+//     const n = parseInt(Math.random() * 255);
+//     return n;
+// }
+
+
+
+// function clicouBotao() {
+//     const r = geraInteiro();
+//     const g = geraInteiro();
+//     const b = geraInteiro();
+    
+//     console.log('entrei na função');
+//     body.style.backgroundColor = 'rgb(' + r + ',' + g + ' , ' + b + ')'
+    
+    
+// }
+
+
 // ------------------------------------------------------------------------
 // 2. melhorando o código
-// [] remover repetição de código
-// [] anonymous function
-// [] arrow function
+// [x] remover repetição de código
+// [x] anonymous function
+// [x] arrow function
 // [] template string
+
+console.log("olar");
+
+const body = document.body;
+console.log(body);
+
+const botao = document.getElementById('trocaCor');
+body.style.backgroundColor = "purple"
+
+botao.addEventListener("click", () => {
+    const r = geraInteiro(255);
+    const g = geraInteiro(255);
+    const b = geraInteiro(255);
+    
+    console.log('entrei na função');
+    body.style.backgroundColor = 'rgb(' + r + ',' + g + ' , ' + b + ')'
+});
+
+geraInteiro = num => parseInt(Math.random() * num);
+
+
+
+
+
